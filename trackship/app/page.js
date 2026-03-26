98499e5cfd796832ad91a32e291c4a5af6430826
kamal <kamalkharel2002@gmail.com>
2026-03-26 10:28:11 +0600

    project structure

---
+++ trackship/app/page.js
@@ -0,0 +1,10 @@
+// pharmacy/app/page.js
+
+import HeroSection from "@/components/home/HeroSection";
+export default function HomePage() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+}
