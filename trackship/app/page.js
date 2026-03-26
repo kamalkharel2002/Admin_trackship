dcc76806bda4f4aa99de678fdcf77da9d76da6af
Tshewang <02210236.cst@rub.edu.bt>
2026-03-26 12:00:47 +0600

    usersection

--- trackship/app/page.js
+++ trackship/app/page.js
@@ -1,10 +1,12 @@
 // pharmacy/app/page.js
 
 import HeroSection from "@/components/home/HeroSection";
+import UserSection from "@/components/home/UserSection";
 export default function HomePage() {
   return (
     <>
       <HeroSection />
+      <UserSection />
     </>
   );
 }
